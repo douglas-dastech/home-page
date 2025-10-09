4d3daab2fefccade40d00cb4cf41a4f82bb2fcff
dastech <contato@dastech.inf.br>
2025-10-09 00:52:02 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-function toggleMenu(){
-  const nav = document.querySelector('.nav');
-  if(!nav) return;
-  const style = getComputedStyle(nav);
-  if(style.display === 'none'){ nav.style.display = 'flex'; nav.style.flexDirection='column'; }
-  else { nav.style.display = 'none'; }
-}
